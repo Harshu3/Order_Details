323ce0ffd9e9007372aadafa83a422f6aec257fe
Harshita <chandaliyaharshita222@gmail.com>
2023-07-23 20:27:19 +0530

    Added Welcome Message

---
+++ Order_Details/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Order_Details
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to SQL Queries Problem!");
+        }
+    }
+}
